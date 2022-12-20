8d85e1b6bd4fb56b40e59eb2b3b1805b9fcc3d74
Robert Olson <featuringbirds18@gmail.com>
2022-12-19 19:47:07 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -56,4 +56,31 @@ app.use(
     next(err);
   });
 
+  const { ValidationError } = require('sequelize');
+
+
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
+
   module.exports = app;
